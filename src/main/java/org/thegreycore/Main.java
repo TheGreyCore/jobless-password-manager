293403229e1558f22505955938c43cf2fc117d70
Dmitri <dmiti.matetski@gmail.com>
2025-01-28 21:50:35 +0200

    Added insert function and corrected mistake in logo

--- src/main/java/org/thegreycore/Main.java
+++ src/main/java/org/thegreycore/Main.java
@@ -1,12 +1,10 @@
 package org.thegreycore;
 
-import org.thegreycore.dto.GetEntryDTO;
 import org.thegreycore.dto.NewEntryDTO;
 import org.thegreycore.config.VaultConfig;
 import org.thegreycore.service.VaultService;
 
 import java.io.File;
-import java.util.List;
 import java.util.Scanner;
 import java.util.logging.Logger;
 
@@ -15,14 +13,16 @@ public class Main {
     public static final Logger LOGGER = Logger.getLogger(Main.class.getName());
 
     private static final String LOGO = """
-                 ______  ___  __   ____________   \s
-             __ / / __ \\/ _ \\/ /  / __/ __/ __/   \s
-            / // / /_/ / ___/ /__/ _/_\\ \\_\\ \\     \s
-            \\___/\\____/_/  /____/___/___/___/_____\s
-              /  |/  / _ | / |/ / _ |/ ___/ __/ _ \\
-             / /|_/ / __ |/    / __ / (_ / _// , _/
-            /_/  /_/_/ |_/_/|_/_/ |_\\___/___/_/|_|\s
-                                                  \s
+                   ______  ____  __    ________________   \s
+                  / / __ \\/ __ )/ /   / ____/ ___/ ___/   \s
+             __  / / / / / __  / /   / __/  \\__ \\\\__ \\    \s
+            / /_/ / /_/ / /_/ / /___/ /___ ___/ /__/ /    \s
+            \\____/\\____/_____/_____/_____//____/____/_____\s
+               /  |/  /   |  / | / /   | / ____/ ____/ __ \\
+              / /|_/ / /| | /  |/ / /| |/ / __/ __/ / /_/ /
+             / /  / / ___ |/ /|  / ___ / /_/ / /___/ _, _/\s
+            /_/  /_/_/  |_/_/ |_/_/  |_\\____/_____/_/ |_| \s
+                                                          \s
             """;
 
     private static final String MENU = """
@@ -49,21 +49,20 @@ public class Main {
 
         System.out.printf("Found vault file: %s %n", f.getAbsolutePath());
         System.out.print("Please provide a master key: ");
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); // Single Scanner instance
         char[] masterKey = scanner.nextLine().toCharArray();
-        List<GetEntryDTO> listOfEntries = vault.getListOfEntries(masterKey);
-        listOfEntries.forEach(System.out::println);
+        showListOfEntries(masterKey);
 
         boolean exit = false;
         while (!exit) {
             System.out.println(MENU);
-            String command = scanner.nextLine();
+            String command = scanner.nextLine(); // Read command
             switch (command.toLowerCase()) {
                 case "a", "add":
-                    addEntry();
+                    addEntry(scanner); // Pass scanner to method
                     break;
                 case "g", "get":
-                    getPassword();
+                    getPassword(scanner); // Pass scanner to method
                     break;
                 case "r", "remove":
                     removeEntry();
@@ -76,18 +75,29 @@ public class Main {
                     break;
             }
         }
+        scanner.close(); // Close scanner only at the end
     }
 
     private static void removeEntry() {
         throw new UnsupportedOperationException();
     }
 
-    private static void addEntry() {
-        throw new UnsupportedOperationException();
+    private static void showListOfEntries(char[] masterKey) {
+        vault.getListOfEntries(masterKey).forEach(System.out::println);
     }
 
-    private static void getPassword() {
-        Scanner scanner = new Scanner(System.in);
+    // Updated methods to reuse the main Scanner
+    private static void addEntry(Scanner scanner) {
+        System.out.println("Please provide a master key: ");
+        char[] masterKey = scanner.nextLine().toCharArray();
+        System.out.println("Please provide service name, username and password separated by a space");
+        String[] data = scanner.nextLine().split(" ");
+        NewEntryDTO entryDTO = new NewEntryDTO(data[0], data[1], data[2], masterKey);
+        vault.addEntry(entryDTO);
+        showListOfEntries(masterKey);
+    }
+
+    private static void getPassword(Scanner scanner) {
         System.out.println("Enter ID to get the password:");
         int id = Integer.parseInt(scanner.nextLine());
         System.out.println("Enter master key:");
